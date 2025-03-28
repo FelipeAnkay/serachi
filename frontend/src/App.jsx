5728450c46241d6dea4ce6795aa2f66527595758
Felipe Ossandon <felipe@ideaup.cl>
2025-03-28 15:11:37 -0500

    Signup en proceso

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import {Routes, Route} from 'react-router-dom';
-import FloatingShape from "./components/floatingShape";
+import FloatingShape from "./components/FloatingShape";
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
-
 
 function App() {
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-blue-900 to-emerald-950 flex items-center justify-center relative overflow-hidden">
-      <FloatingShape color="bg-green-500" size="w-64 h-64" top="-5%" left="10%" delay={0}/>
-      <FloatingShape color="bg-emerald-500" size="w-48 h-48" top="70%" left="80%" delay={5}/>
-      <FloatingShape color="bg-lime-500" size="w-32 h-32" top="40%" left="-10%" delay={2}/>
+    <div
+      className='min-h-screen bg-gradient-to-br from-gray-900 via-blue-900 to-emerald-950 flex items-center justify-center relative overflow-hidden'
+    >
+      <FloatingShape color="bg-green-500" size="w-64 h-64" top="-5%" left="10%" delay={0} />
+      <FloatingShape color="bg-emerald-500" size="w-48 h-48" top="70%" left="80%" delay={5} />
+      <FloatingShape color="bg-lime-500" size="w-32 h-32" top="40%" left="-10%" delay={2} />
 
       <Routes>
         <Route path="/" element={"Home"} />
@@ -18,7 +19,8 @@ function App() {
         <Route path="/login" element={<LoginPage/>} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
