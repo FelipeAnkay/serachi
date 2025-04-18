e22095a2ef52c8abb61b9f2f1d171c9d9a7c6c30
Felipe Ossandon <felipe@ideaup.cl>
2025-04-18 13:38:28 -0500

    sync files

--- backend/index.js
+++ backend/index.js
@@ -4,20 +4,26 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from 'cookie-parser';
+import path from "path";
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.get("/", (req,res) =>{
-    res.send("Hello W123");
-});
 app.use(cors({origin: process.env.CLIENT_URL, credentials:true}));
+    
 app.use(express.json());  //Allows us to parse incoming request with JSON
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+    })
+}
 
 app.listen(PORT, () =>{
     connectDB();
