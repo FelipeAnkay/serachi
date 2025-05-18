75ece0e49d29a565685125e340d8d369139e49b4
Felipe Ossandon <felipe@ideaup.cl>
2025-05-18 16:02:24 -0500

    Reservations fix and Create Reservation on Confirmed Quotes

--- frontend/src/pages/settings/SetStaff.jsx
+++ frontend/src/pages/settings/SetStaff.jsx
@@ -7,6 +7,7 @@ import { useStaffServices } from '../../store/staffServices';
 import languagesList from '../../components/languages.json'
 
 
+
 const SetStaff = () => {
     const { getStaffList, createStaff, updateStaff, getStaffEmail, removeStaff } = useStaffServices();
     const storeId = Cookies.get('storeId');
